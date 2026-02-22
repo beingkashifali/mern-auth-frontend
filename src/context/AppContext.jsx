a37e865f85747bede357c21d264da3f2fe19be7b
kashifkhalid3551 <kashifali7519181@gmail.com>
2026-02-22 15:31:12 +0500

    updated url

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -8,7 +8,7 @@ function AppContextProvider(props) {
   axios.defaults.withCredentials = true;
 
   // const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const backendUrl = "https://mern-authentication-backend-hy1r.onrender.com";
+  const backendUrl = "https://mern-auth-backend-2-b832.onrender.com";
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
 
