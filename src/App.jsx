acdfa0c0a9fb997786dbbc8fad66b0e6730f69fc
kashifkhalid3551 <kashifali7519181@gmail.com>
2026-02-22 14:07:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify-email" element={<EmailVerify />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContent = createContext();
+
+function AppContextProvider(props) {
+  axios.defaults.withCredentials = true;
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.post(backendUrl + "/api/auth/is-auth");
+
+      if (data.success) {
+        setIsLoggedIn(true);
+        await getUserData();
+      }
+    } catch (error) {
+      console.log(error.response.data.msg);
+    }
+  };
+
+  useEffect(() => {
+    getAuthState();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+
+      data.success ? setUserData(data.userData) : toast.error(data.msg);
+    } catch (error) {
+      toast.error(error.response.data.msg);
+    }
+  };
+
+  const value = {
+    backendUrl,
+    isLoggedIn,
+    setIsLoggedIn,
+    userData,
+    setUserData,
+    getUserData,
+  };
+
+  return (
+    <AppContent.Provider value={value}>{props.children}</AppContent.Provider>
+  );
+}
+
+export default AppContextProvider;
